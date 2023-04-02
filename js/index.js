4136e909529694f58e002122098f6ec1dd29efce
Kaushal Mishra <kaushalm.21301@gmail.com>
2023-04-02 18:54:37 +0530

    done Commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,23 @@
-
-Data.forEach(element,i => {
-
+data.forEach((element, i) => {
+  document.querySelector(".parent_grid").innerHTML += `
+  <div class="child_grid ${data[i].id_name}">
+  <div class="user_profile">
+    <img
+      src="${data[i].profile_image}"
+      class="rounded-circle img-fluid "
+      alt="${data[i].id_name}_profile"
+    />
+    <div class="user_detail">
+      <h6> ${data[i].name}</h6>
+      <p> ${data[i].status}</p>
+    </div>
+  </div>
+  <h1>
+  ${data[i].title}
+  </h1>
+  <p>
+  ${data[i].discription}
+  </p>
+</div>
+  `;
 });
